95f4c8f3936b011cc271b46839ff0edb97382291
[B-0916] <[ bishakhanapit16@gmail.com]>
2025-06-26 22:31:46 +0545

    new project

---
+++ switchcase.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+    char op = '+';
+
+    switch(op) {
+        case '+':
+            printf("Sum = %d\n", a + b);
+            break;
+        case '-':
+            printf("Difference = %d\n", a - b);
+            break;
+        case '*':
+            printf("Product = %d\n", a * b);
+            break;
+        case '/':
+            printf("Quotient = %d\n", a / b);
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+
+    return 0;
+}
